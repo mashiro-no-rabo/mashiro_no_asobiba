e345ba0d62289213899ce44951cbd86b25e5a974
AquarHEAD Lou <aquarhead@ela.build>
2018-05-16 12:04:20 +0000

    Test The Forge market

--- lib/esi.ex
+++ lib/esi.ex
@@ -1,25 +1,33 @@
 defmodule ESI.Client do
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, "https://esi.tech.ccp.is/latest/universe"
-  plug Tesla.Middleware.Headers, %{
-    "User-Agent" => "eve_gamedesign",
-  }
+  plug Tesla.Middleware.BaseUrl, "https://esi.evetech.net/latest"
+  plug Tesla.Middleware.Headers, %{"User-Agent" => "mashiro-no-asobiba"}
   plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.Retry, delay: 2000, max_retries: 20
 
   adapter Tesla.Adapter.Hackney
 
   def get_systems do
-    get("/systems/")
+    get("/universe/systems/")
     |> Map.get(:body)
   end
 
   def get_system(system_id) do
-    ss = get("/systems/#{system_id}/")
+    name = get("/universe/systems/#{system_id}/")
     |> Map.get(:body)
-    |> Map.get("security_status")
+    |> Map.get("name")
 
-    {system_id, ss}
+    {system_id, name}
+  end
+
+  def get_market_page(region_id, page) do
+    get("/markets/#{region_id}/orders/?page=#{page}")
+  end
+
+  def get_orders(region_id, page) do
+    get_market_page(region_id, page)
+    |> Map.get(:body)
   end
 end
 
@@ -30,5 +38,18 @@ defmodule ESI do
     stream
     |> Enum.map(fn {:ok, v} -> v end)
     |> Enum.into(%{})
+  end
+
+  def test_orders do
+    first_page = ESI.Client.get_market_page(10000002, 1)
+
+    pages = String.to_integer(first_page.headers["x-pages"])
+    stream = Task.async_stream(2..pages, fn page -> ESI.Client.get_market_page(10000002, page) end)
+
+    stream
+    |> Enum.map(fn {:ok, resp} ->
+      String.equivalent?(resp.headers["expires"], first_page.headers["expires"])
+      _ -> :expired
+    end)
   end
 end
